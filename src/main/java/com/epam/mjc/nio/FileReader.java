603852ace6efbad73650c09b98f43915852387e0
MuhammadJon <mtjohn9411@gmail.com>
2023-03-29 15:09:06 +0500

    Stage 1 Task NIO File uploading solution has been developed

--- src/main/java/com/epam/mjc/nio/FileReader.java
+++ src/main/java/com/epam/mjc/nio/FileReader.java
@@ -2,30 +2,30 @@ package com.epam.mjc.nio;
 
 import java.io.File;
 import java.io.IOException;
-import java.nio.file.Files;
+import java.io.RandomAccessFile;
 import java.util.HashMap;
-import java.util.List;
 import java.util.Map;
 
 
 public class FileReader {// Compliant
+
     public Profile getDataFromFile(File file) {
         Map<String, Object> map = new HashMap<>();
-        List<String> allLines = null;
-        try {
-            allLines = Files.readAllLines(file.toPath());
+
+        try (RandomAccessFile raf = new RandomAccessFile(file, "r")){
+
+            String str;
+
+            while ((str = raf.readLine()) != null) {
+                String[] keyValuePair = str.split(":");
+                if (keyValuePair.length > 1) {
+                    String key = keyValuePair[0];
+                    String value = keyValuePair[1];
+                    map.put(key, value.replace(" ", ""));
+                }
+            }
         } catch (IOException ex) {
             System.err.println("File not found or error is: " + ex);
-        }
-
-        for (String line : allLines) {
-            System.out.println(line);
-            String[] keyValuePair = line.split(":");
-            if (keyValuePair.length > 1) {
-                String key = keyValuePair[0];
-                String value = keyValuePair[1];
-                map.put(key, value.replace(" ", ""));
-            }
         }
 
         Profile profile = new Profile();
